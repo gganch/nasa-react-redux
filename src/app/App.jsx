ab6a4f26706ed1cee491085f9d680d1a507b79ae
ganchimeg <ganchimeg10@gmail.com>
2017-11-08 17:28:29 -0500

    finished

---
+++ src/app/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import AstronomyContainer from './components/AstronomyContainer';
+
+const App = () => {
+  return (
+    <AstronomyContainer />
+  )
+}
+
+export default App;
+
+/*import cat from 'images/cat';
+import dog from 'images/dog';
+import dogs from 'images/dogs';
+
+const hello = () => {
+  return (
+    <div className="container">
+
+      <div className="image-wrapper">
+        <img src={cat} className="image-wrapper__image" alt=""/>
+      </div>
+
+      <div className="image-wrapper">
+        <img src={dog} className="image-wrapper__image" alt=""/>
+      </div>
+
+      <div className="image-wrapper">
+        <img src={dogs} className="image-wrapper__image" alt=""/>
+      </div>
+
+    </div>
+  )
+}
+
+export default hello;*/
---
+++ src/app/components/AstronomyContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import AstronomyCard from './AstronomyCard';
+import axios from 'axios';
+
+class AstronomyContainer extends  Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            astronomy: []
+        }
+    }
+
+    componentDidMount() {
+        const API_KEY = 'ViX2t89PxuTvq7AALaKPKE7DfEOmCByABzSJ0JGq';
+        const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key='
+
+        axios.get(END_POINT+API_KEY)
+            .then(response => {
+                this.setState({
+                    astronomy: response.data
+                })
+                console.log(this.state.astronomy)
+            })
+            .catch(error => {
+                console.log(error, 'failed to fetch data')
+            });
+    }
+
+    render() {
+        const {astronomy} = this.state;
+        return (
+            <AstronomyCard data={astronomy} />
+        )
+    }
+}
+
+export default AstronomyContainer;
